7c8309085abc325c3f9d1423003b2d10ac87f1e6
KandeSoto <kandelariosoto@gmail.com>
2024-05-29 13:56:36 -0700

    08 importaciones y exportaciones

---
+++ src/bases/08-imp-exp.js
@@ -0,0 +1,17 @@
+// import { heroes } from "./data/heroes";
+//import { heroes } from "./data/heroes";
+import heroes, { owners } from "../data/heroes";
+//console.log(heroes);
+
+console.log(owners);
+
+const getHeroeById = (id) => {
+    return heroes.find((heroe) => heroe.id === id);
+};
+
+console.log(getHeroeById(2));
+
+const getHeroesByOwner = (owner) =>
+    heroes.filter((heroe) => heroe.owner === owner);
+
+console.log(getHeroesByOwner("DC"));
