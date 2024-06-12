5745c62f9a326dd0606a0469870aad00cf6a85e8
KandeSoto <kandelariosoto@gmail.com>
2024-06-12 13:56:24 -0700

    09 promesas

--- src/bases/08-imp-exp.js
+++ src/bases/08-imp-exp.js
@@ -3,15 +3,17 @@
 import heroes, { owners } from "../data/heroes";
 //console.log(heroes);
 
-console.log(owners);
+//console.log(owners);
 
 const getHeroeById = (id) => {
     return heroes.find((heroe) => heroe.id === id);
 };
 
-console.log(getHeroeById(2));
+//console.log(getHeroeById(2));
 
 const getHeroesByOwner = (owner) =>
     heroes.filter((heroe) => heroe.owner === owner);
 
-console.log(getHeroesByOwner("DC"));
+//console.log(getHeroesByOwner("DC"));
+
+export { getHeroeById, getHeroesByOwner };
---
+++ src/data/animes.js
@@ -0,0 +1,39 @@
+const animes = [
+    {
+        id: 1,
+        nombre: "Attack on titan",
+        temporadas: 6,
+        capitulos: 89,
+        estudio: "Mappa",
+    },
+    {
+        id: 2,
+        nombre: "Reincarnation Jobless",
+        temporadas: 2,
+        capitulos: 48,
+        estudio: "Mappa",
+    },
+    {
+        id: 3,
+        nombre: "Saint Seiya",
+        temporadas: 7,
+        capitulos: 220,
+        estudio: "Wit Studio",
+    },
+    {
+        id: 1,
+        nombre: "Naruto",
+        temporadas: 20,
+        capitulos: 570,
+        estudio: "Universal",
+    },
+    {
+        id: 1,
+        nombre: "Mashle muscle",
+        temporadas: 2,
+        capitulos: 24,
+        estudio: "Wit Studio",
+    },
+];
+
+export default animes;
